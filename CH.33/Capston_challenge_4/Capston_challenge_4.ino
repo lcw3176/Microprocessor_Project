0568ed4d62b29a571355e1b1f82bed6191b0ec7a
joebrooks <lcw3176@naver.com>
2021-06-06 11:32:10 +0900

    챌린지 4 추가

---
+++ CH.33/Capston_challenge_4/Capston_challenge_4.ino
@@ -0,0 +1,52 @@
+#include <LiquidCrystal.h>
+#include <TimerOne.h>
+#include <MsTimer2.h>
+
+#define buzzer 22
+long cnt = 0;
+bool buzzer_state = true;
+char buzzer_count = 0;
+
+LiquidCrystal lcd(23, 24, 25, 26, 27, 28);
+
+void setup() {
+  pinMode(buzzer, OUTPUT);   
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+
+  Timer1.initialize(200);
+  Timer1.attachInterrupt(counterCallBack);
+  
+  Serial.begin(9600);
+}
+
+void counterCallBack()
+{
+  cnt++;
+}
+
+void buzzerBeep()
+{
+  buzzer_count++;
+  digitalWrite(buzzer, buzzer_state);
+  buzzer_state = !buzzer_state;
+
+  if(buzzer_count >= 2)
+  {
+    buzzer_count = 0;
+    MsTimer2::stop();
+  }
+}
+
+void loop() {
+//  Serial.println(cnt);
+  lcd.print(cnt);
+  lcd.setCursor(0, 0);
+  
+  if(cnt % 10000 == 0)
+  {
+    Serial.println(cnt);
+    MsTimer2::set(1000, buzzerBeep);
+    MsTimer2::start();
+  }
+}

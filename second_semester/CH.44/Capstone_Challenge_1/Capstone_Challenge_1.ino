b43eb0bd9180d6dc40c0caf6465d2db872c9d5b0
lcw3176 <lcw3176@naver.com>
2021-10-28 15:16:24 +0900

    코드 추가

--- second_semester/CH.44/Capstone_Challenge_1/Capstone_Challenge_1.ino
+++ second_semester/CH.44/Capstone_Challenge_1/Capstone_Challenge_1.ino
@@ -1,9 +1,46 @@
-void setup() {
-  // put your setup code here, to run once:
+#define ledOne 22
+#define ledTwo 24
+#define ledThree 26
+#define ledFour 28
+#define ledFive 30
+#define ledSix 32
+#define ledSeven 34
+#define ledEight 36
 
+const int sampleWindow = 50;
+unsigned int sample;
+
+void setup() {
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  unsigned long startMillis = millis();
+  unsigned int peakToPeak = 0;
 
+  unsigned int signalMax = 0;
+  unsigned int signalMin = 1024;
+
+  while(millis() - startMillis < sampleWindow){
+    sample = analogRead(A1);
+
+    if(sample > signalMax){
+      signalMax = sample;
+    } else if(sample < signalMin){
+      signalMin = sample;
+    }
+  }
+
+  peakToPeak = signalMax - signalMin;
+  double volts = (peakToPeak * 3.3) / 1024;
+
+  if(volts > 1.0){
+    for(int i = ledOne; i <= ledEight; i += 2){
+      digitalWrite(i, LOW); 
+    }
+    
+    for(int i = ledOne; i <= map(volts, 0, 3.3, ledOne, ledEight); i += 2){
+      digitalWrite(i, HIGH);  
+    }
+  }
 }
